db495ec4005224b32a0789def8a157628ba1300a
Satyam Prajapati <satyamm1402@gmail.com>
2025-08-03 21:26:12 +0530

    auth setup: - added google and github oauth - middleware.ts

---
+++ src/components/sign-in.tsx
@@ -0,0 +1,15 @@
+
+import { signIn } from "@/services/auth"
+ 
+export default function SignIn({provider}: {provider: string}) {
+  return (
+    <form
+      action={async () => {
+        "use server"
+        await signIn(provider)
+      }}
+    >
+      <button type="submit">Signin with {provider}</button>
+    </form>
+  )
+} 
--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -1,9 +1,12 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import z from "zod";
+import { config } from "dotenv";
+config({ path: ".env.local" })
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string().min(1),
+    AUTH_SECRET: z.string().min(1),
   },
   experimental__runtimeEnv: process.env
 })
