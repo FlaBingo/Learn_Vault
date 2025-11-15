41f6016cf9730ff70d2a636beaef672cf85de417
Satyam Prajapati <satyamm1402@gmail.com>
2025-11-15 20:14:43 +0530

    allow every image url that has https protocol

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'media.istockphoto.com'
       },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ]
   }
 };
